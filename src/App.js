ca99d131324610331d4c1fa0a918c8527463f993
Bent <bent.stevens@student.howest.be>
2022-06-03 14:35:38 +0200

    made a working camera component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Camera from './components/Camera';
 
 function App() {
   return (
     <div className="App">
-      <p>TEST</p>
+      <Camera />
     </div>
   );
 }
---
+++ src/components/Camera.js
@@ -0,0 +1,35 @@
+import React, { useRef, useState, useEffect } from 'react';
+
+const Camera = () => {
+
+    const videoRef = useRef(null);
+
+    const getVideo = () => {
+        navigator.mediaDevices
+            .getUserMedia({
+                video: { width: 1920, height: 1080 }
+            })
+            .then(stream => {
+                let video = videoRef.current;
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(err => {
+                console.error(err);
+            })
+    }
+
+    useEffect( () => {
+        getVideo();
+    }, [videoRef])
+
+    return (
+        <div>
+            <div className="camera">
+                <video ref={videoRef}></video>
+            </div>
+        </div>
+    );
+};
+
+export default Camera;
