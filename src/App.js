1d78479434d00bdb5493b8a8ad891f455c4d3a51
Bent <bent.stevens@student.howest.be>
2022-06-07 11:25:42 +0200

    added a record video feature #4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Camera from './components/Camera';
 import AddVideo from './components/AddVideo';
+import RecordVideo from './components/RecordVideo';
+
 
 function App() {
   return (
     <div className="App">
-      <AddVideo />
-      <Camera />
+      {/* <AddVideo /> */}
+      {/* <Camera /> */}
+      <RecordVideo />
     </div>
   );
 }
---
+++ src/components/RecordVideo.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import VideoRecorder from 'react-video-recorder'
+
+
+const RecordVideo = () => {
+
+    return (
+        <div className="record">
+            <VideoRecorder
+                chunkSize={(250)}
+                constraints={{
+                    audio: true,
+                    video: true
+                }}
+                timeLimit={30000}
+                isOnInitially
+
+                    //Replay controls and video not on loop 
+                replayVideoAutoplayAndLoopOff
+                showReplayControls
+
+                onRecordingComplete={(videoBlob) => {
+                    // Do something with the video...
+                    console.log('videoBlob', videoBlob)
+                }}
+                
+            /> 
+
+
+        </div>
+    );
+};
+
+export default RecordVideo;
