c598c7e32eb06338008e40ffc6664078704e6a61
VanBelleghemMarie <marie.van.belleghem@student.howest.be>
2022-06-10 19:48:46 +0200

    removed console.logs

--- src/components/FullVideo.js
+++ src/components/FullVideo.js
@@ -7,26 +7,20 @@ const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 const VideoCard = () => {
     const { videoId } = useParams();
-    console.log(videoId);
-
 
     const { isLoading, error, data: video } = useQuery(["video", videoId], async () => {
         const data = await fetch(`${backendUrl}/api/videos/${videoId}?populate=*`).then(r => r.json());
-        console.log(data);
         return data;
     });
 
-    console.log(video);
     return (
         <article>
             {error && <p>Video could not be loaded</p>}
             {isLoading && <p>Video loading...</p>}
             <Card >
-
                 {video && (<>
                     {video.data.attributes.video.data &&
-                        //     <AdvancedVideo cldVid={video.data.attributes.video.data.attributes.url} controls autoplay poster={video.data.attributes.video.data.attributes.url} />
-                        <ReactPlayer
+                           <ReactPlayer
                             url={video.data.attributes.video.data.attributes.url}
                             width='100vw'
                             height='100vh'
@@ -35,7 +29,6 @@ const VideoCard = () => {
 
                         />
                     }
-
                 </>)}
             </Card>
         </article>);
