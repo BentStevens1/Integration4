7ee618d1b1d92afb4244e18e8209ff8854047718
VanBelleghemMarie <marie.van.belleghem@student.howest.be>
2022-06-13 14:43:34 +0200

    fixed styling for button

--- src/views/Home.js
+++ src/views/Home.js
@@ -16,7 +16,7 @@ function Home() {
       <Box sx={{height: '100vh', width:'100vw', display: 'flex', flexDirection: 'column' , alignItems: 'center', justifyContent: 'space-around'}}>
           <Typography>...Video...</Typography>
           
-          <Link to={`/pageOne`}><Button variant="contained" color="secondary" sx={{padding: "1rem 4rem", margin:"2rem"}}>Verder</Button></Link>
+          <Link to={`/pageOne`}><Button variant="contained" color="secondary" sx={{padding: "1rem 4rem", margin:"2rem", color:'text.secondary'}}>Verder</Button></Link>
       </Box>
 
     
