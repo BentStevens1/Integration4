13442f79e1d4c2683ab0e8f662c39d2f803bfdfd
VanBelleghemMarie <marie.van.belleghem@student.howest.be>
2022-06-13 17:30:04 +0200

    added styling

--- src/components/AddWord.js
+++ src/components/AddWord.js
@@ -1,83 +1,83 @@
-import { Alert,Snackbar, Stack, TextField, Typography, Button } from '@mui/material';
+import { Alert, Snackbar, Stack, TextField, Typography, Button, Box, CardMedia} from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 import { useForm } from "react-hook-form";
 import { useMutation, useQueryClient } from 'react-query';
 import { Link, useNavigate } from "react-router-dom";
 import { styled } from '@mui/material/styles';
+import '../index.css';
 
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
-
-
-
 const defaultValues = {
-  content: '',
-  score: '1',
+    content: '',
+    score: '1',
 };
 
 const Input = styled('input')({
-  display: 'none',
+    display: 'none',
 });
 
 
 const AddWord = () => {
-  const { handleSubmit, formState: { errors }, register, control, reset, watch } = useForm({ defaultValues });
-  const queryClient = useQueryClient()
-  const navigate = useNavigate();
+    const { handleSubmit, formState: { errors }, register, control, reset, watch } = useForm({ defaultValues });
+    const queryClient = useQueryClient()
+    const navigate = useNavigate();
 
-  const postWord = async (data) => {
-    const formData = new FormData();
-    console.log(data);
-    formData.append("data", JSON.stringify({ ...data, video: null }))
-    return await fetch(`${backendUrl}/api/words`, {
-      method: "POST",
-      body: formData,
-    }).then(r => r.json());
-  }
+    const postWord = async (data) => {
+        const formData = new FormData();
+        console.log(data);
+        formData.append("data", JSON.stringify({ ...data, video: null }))
+        return await fetch(`${backendUrl}/api/words`, {
+            method: "POST",
+            body: formData,
+        }).then(r => r.json());
+    }
 
-  const mutation = useMutation(postWord, {
-    onSuccess: () => {
-      console.log("success")
-      queryClient.invalidateQueries('words');
-      reset()
-    },
-  })
+    const mutation = useMutation(postWord, {
+        onSuccess: () => {
+            console.log("success")
+            queryClient.invalidateQueries('words');
+            reset()
+        },
+    })
 
-  const onSubmit = data => {
-    mutation.mutate(data);
-    navigate('/pageThree');
-  }
+    const onSubmit = data => {
+        mutation.mutate(data);
+        navigate('/pageThree');
+    }
 
-  const handleCloseSnackbar = () => {
-    mutation.reset();
-  }
+    const handleCloseSnackbar = () => {
+        mutation.reset();
+    }
 
 
-  return (
-    <Stack spacing={4} sx={{ flex: 1 }} as="form" noValidate onSubmit={handleSubmit(onSubmit)}>
-      <TextField sx={{ input: { color: 'white' } }}
-        id="content"
-        label="Vul het woord in"
-        variant="filled"
-        error={!!errors?.title}
-        helperText={errors?.title?.message}
-        {...register("content", {
-        })}
-      />
+    return (
+        <Stack class='input' spacing={4} as="form" noValidate onSubmit={handleSubmit(onSubmit)} >
+            <TextField sx={{ input: { color: 'text.secondary', fontFamily:'Bebas Neue'} }}
+                id="content"
+                label="Schrijf jouw woord hier..."
+                variant="standard"
+                fontFamily="Bebas Neue"
+                error={!!errors?.title}
+                helperText={errors?.title?.message}
+                {...register("content", {
+                })}
+            />
 
-<Link to={`/pageFour`}><Button>overslaan</Button></Link>
-      <LoadingButton loading={mutation.isLoading} color="secondary"
-        loadingIndicator="Adding video" type="submit" variant="contained">Verder</LoadingButton>
-      <Snackbar open={mutation.isSuccess} anchorOrigin={{ vertical: "bottom", horizontal: "right" }} autoHideDuration={3000} onClose={handleCloseSnackbar}>
-        <Alert severity="success" sx={{ width: '100%' }}>
-          Word added
-        </Alert>
-        
+            <Box>
+                <Link to={`/pageFour`}><Button sx={{marginTop:"3rem", width:'14rem', color:'text.primary', fontFamily:'Bebas Neue'}}>overslaan</Button></Link>
+                <LoadingButton loading={mutation.isLoading} color="secondary"
+                    loadingIndicator="Adding word" type="submit" variant="contained" sx={{marginTop:"3rem", width:'14rem', padding:'.5rem', fontFamily:'Bebas Neue'}}>Verder</LoadingButton>
+                <Snackbar open={mutation.isSuccess} anchorOrigin={{ vertical: "bottom", horizontal: "right", fontFamily:'Bebas Neue'}} autoHideDuration={3000} onClose={handleCloseSnackbar}>
+                    <Alert severity="success" sx={{ width: '100%' }}>
+                        Woord ontvangen
+                    </Alert>
+                </Snackbar>
+            </Box>
 
-      </Snackbar>
-    </Stack>
-  );
+        </Stack >
+    );
 }
 
 export default AddWord;
