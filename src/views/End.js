e8429c4fefc65fe3dae5eff7e5379741a0e59b0b
VanBelleghemMarie <marie.van.belleghem@student.howest.be>
2022-06-17 23:13:06 +0200

    added qr code to figma to end page

--- src/views/End.js
+++ src/views/End.js
@@ -1,6 +1,6 @@
 import { Paper } from "@mui/material";
 import { styled } from '@mui/material/styles';
-import { Typography, Button, Box } from '@mui/material';
+import { Typography, Button, Box, CardMedia, Card } from '@mui/material';
 import { Link } from "react-router-dom";
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -14,14 +14,14 @@ const Item = styled(Paper)(({ theme }) => ({
 function End() {
     return (
         <Box sx={{ height: '100vh', width: '100vw', padding: "3rem" }}>
-            <Box className="title2" sx={{ display: "flex", flexDirection: "row", gap: "1rem", alignItems: "center", color: "text.secondary" }}>
+            <Box className="title2" sx={{ display: "flex", flexDirection: "row", gap: "1rem", alignItems: "center", justifyContent:"space-evenly", color: "text.secondary" }}>
                 <Typography fontSize={"13rem"} fontFamily="Bebas Neue" >Bedankt</Typography>
                 <Typography fontSize={"3rem"} fontFamily="Bebas Neue" >voor jouw medewerking</Typography>
             </Box>
-            <Box sx={{ display: "flex", alignItems: "center", gap:"2rem", flexDirection: "column" }}>
+            <Box sx={{ display: "flex", alignItems: "center", gap: "2rem", flexDirection: "column" }}>
                 <Typography fontFamily="Bebas Neue" variant="h2" margin={"3rem"} textAlign={"center"}>Volg ons op sociale media</Typography>
-                <Typography>...qr-code...</Typography>
-                <Typography fontFamily="Bebas Neue" textAlign={"center"} sx={{fontSize: "2rem"}}>spread the sticker and the message</Typography>
+                <Card className="qr" sx={{borderRadius: "50px"}}></Card>
+                <Typography fontFamily="Bebas Neue" textAlign={"center"} sx={{ fontSize: "2rem" }}>spread the sticker and the message</Typography>
                 <Link to={`/`}><Button sx={{ fontFamily: "Bebas Neue", alignItems: "center" }}>return to start</Button></Link>
             </Box>
         </Box>
