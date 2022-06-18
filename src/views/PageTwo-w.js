96508c00a767c7a690100d1d26d3266ed69aefa7
VanBelleghemMarie <marie.van.belleghem@student.howest.be>
2022-06-18 15:09:42 +0200

    changed code so only words with videos show up

--- src/views/PageTwo-w.js
+++ src/views/PageTwo-w.js
@@ -1,44 +1,60 @@
 import { Typography, Box, Button } from '@mui/material';
 import CheckboxesGroup from '../components/SelectWords';
 import { useQuery } from "react-query";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 
-function PageTwo2(){
+function PageTwo2() {
+
+    const wordsWithVideo = []
 
     const { isLoading, data: words } = useQuery("words", async () => {
         const data = await fetch(`${backendUrl}/api/words?populate=*`).then(r => r.json());
         const words = data.data;
-        words.sort((a, b) => Math.random() > 0.5 ? -1 : 1);
-        return words.slice(0, 6);
+        // words.sort((a, b) => Math.random() > 0.5 ? -1 : 1);
+        // return words.slice(0, 6);
+         return words;
     });
 
-  const videoid = 3;
+    if (!isLoading) {
+        words.forEach(word => {
+            if (word.attributes.videos.data.length != 0 && wordsWithVideo.length < 6) {
+                wordsWithVideo.push(word);
+            }
+        })
+    }
 
 
-  console.log(words);
-    var style= {
+
+
+
+    console.log(words);
+    var style = {
         width: '40vh',
         height: '10vh',
-        fontSize: "40px", 
+        fontSize: "40px",
         border: "none",
         backgroundColor: "white",
         color: "1C6F81",
         margin: "2rem",
         fontFamily: "Inter, Arial",
         fontweight: "700",
-        borderRadius: "12px", 
+        borderRadius: "12px",
         alignSelf: "flex-end"
     }
     return (
         <>
-            <Box sx={{height: '100vh', width:'100vw', display: 'flex', flexDirection: 'column' , alignItems: 'center', justifyContent: 'space-around'}}> 
+            <Box sx={{ height: '100vh', width: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around' }}>
                 <Typography fontFamily={"bebas Neue"} variant='h3' color="secondary" sx={{ textAlign: 'center' }} >Selecteer de term die het meest wordt gebruikt in jouw omgeving:</Typography>
                 {words && (<>
-               <CheckboxesGroup words={words} />
-               </>)}
+
+                    {
+                        console.log(wordsWithVideo)
+                    }
+                    <CheckboxesGroup words={wordsWithVideo} />
+                </>)}
             </Box>
         </>
     )
