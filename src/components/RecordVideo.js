934b5b4a13aab8004130a2d5dbd9c948c51f13b7
VanBelleghemMarie <marie.van.belleghem@student.howest.be>
2022-06-18 21:35:44 +0200

    added navigation to next page

--- src/components/RecordVideo.js
+++ src/components/RecordVideo.js
@@ -12,16 +12,17 @@ const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 
 const RecordVideo = () => {
-    
+
     const radio = useStore((state) => state.radio);
     const overlay = useStore(state => state.overlay);
     const setOverlay = useStore(state => state.setOverlay);
-    
+    const navigate = useNavigate();
+
     console.log(radio);
     const [videoBlob, setVideoBlob] = useState(null);
-    const { handleSubmit, reset,  } = useForm();
+    const { handleSubmit, reset, } = useForm();
     const queryClient = useQueryClient()
-    
+
 
 
     const postVideo = async (blob) => {
@@ -45,47 +46,48 @@ const RecordVideo = () => {
     })
 
     const test = () => {
-        mutation.mutate(videoBlob)
+        navigate('/VideoOverview');
+        mutation.mutate(videoBlob);
     }
 
     return (
-        <div className="record">   
-        <Box sx={{ position: 'fixed', zIndex: "0", height:"100vh", width:"100vw"}} >
-            <VideoRecorder
-                chunkSize={(250)}
-                constraints={{
-                    audio: true,
-                    video: true
-                }}
-                // timeLimit={30000}
-                isOnInitially
+        <div className="record">
+            <Box sx={{ position: 'fixed', zIndex: "0", height: "100vh", width: "100vw" }} >
+                <VideoRecorder
+                    chunkSize={(250)}
+                    constraints={{
+                        audio: true,
+                        video: true
+                    }}
+                    // timeLimit={30000}
+                    isOnInitially
 
-                //Replay controls and video not on loop 
-                replayVideoAutoplayAndLoopOff
-                showReplayControls
-                
-                onRecordingComplete={(videoBlob) => {
-                    // Do something with the video...
-                    console.log('videoBlob', videoBlob);
-                    // uploadVideo(videoBlob);
-                    // postVideo(videoBlob);
-                    setVideoBlob(videoBlob);
-                    setOverlay(true)
+                    //Replay controls and video not on loop 
+                    replayVideoAutoplayAndLoopOff
+                    showReplayControls
 
-                }}
-              
-                
-            /> 
-        
-        </Box>    
-            { overlay &&
-            <Stack spacing={4} direction="row" sx={{ position: 'absolute', zIndex: "3", overflowX: 'hidden', bottom:"0", marginBottom:"5rem", marginLeft:"10rem"}} as="form" noValidate onSubmit={handleSubmit(test)}>
-                <LoadingButton color="secondary"
-                    loadingIndicator="Adding video" type="submit" variant="contained" sx={{ color: "black", typography: "h4", textTransform: "uppercase", fontFamily: "Bebas Neue" }}>Upload</LoadingButton>
+                    onRecordingComplete={(videoBlob) => {
+                        // Do something with the video...
+                        console.log('videoBlob', videoBlob);
+                        // uploadVideo(videoBlob);
+                        // postVideo(videoBlob);
+                        setVideoBlob(videoBlob);
+                        setOverlay(true)
+
+                    }}
+
+
+                />
+
+            </Box>
+            {overlay &&
+                <Stack spacing={4} direction="row" sx={{ position: 'absolute', zIndex: "3", overflowX: 'hidden', bottom: "0", marginBottom: "5rem", marginLeft: "10rem" }} as="form" noValidate onSubmit={handleSubmit(test)}>
+                    <LoadingButton color="secondary"
+                        loadingIndicator="Adding video" type="submit" variant="contained" sx={{ color: "black", typography: "h4", textTransform: "uppercase", fontFamily: "Bebas Neue" }}>Upload</LoadingButton>
                     <Link to={`/VideoOverview`}><Button sx={{ color: "black", typography: "h4", textTransform: "uppercase", fontFamily: "Bebas Neue" }}>Toch geen video opnemen</Button></Link>
-            </Stack>
-            }   
-    
+                </Stack>
+            }
+
 
 
         </div>
